98fca9c3457de9c865c03cb2c42fa506ea82bfca
Matt Wetmore <wetmore.matt@gmail.com>
2013-10-06 12:59:22 -0400

    fix the arrow

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,10 +1,4 @@
 $(function() {
-  
-  var map = {
-    'inane': 141,
-    'watercooler': 531,
-    'pseudo': 925
-  };
 
   var iconMap = {
     'lol': 'github',
@@ -13,11 +7,25 @@ $(function() {
     'weather': 'cloud'
   };
 
+  var which = '#watercooler';
+
   $('#choices .button').click(function(e) {
-    $('#triangle').show();
-    var which = e.target.id;
+    var $t = $('#triangle');
+    if ($t.hasClass('gone')) {
+      $t.removeClass('gone');
+      $t.animate({ 'margin-top': '-24px' });
+    }
+    which = e.target.id;
     $('.dimmer').removeClass('inactive').addClass('active');
-    $('#triangle').animate({ 'margin-left': map[which] + 'px' });
+
+    // slide the arrow the right place
+    var $button = $('#' + which);
+    var left = $button.offset().left - $('#speech-bubble').offset().left
+    var bw = $button.css('width');
+    var width = parseInt(bw.substr(0, bw.length - 2), 10);
+    var newpos = left - 40 + width / 2;
+
+    $('#triangle').animate({ 'margin-left': newpos + 'px' });
 
     var promise = $.getJSON('/smalltalk', {
       level: which
@@ -25,12 +33,20 @@ $(function() {
 
     promise.done(function(data) {
       $('#speech-bubble .content').empty().html(data.text);
-      console.log(data);
       $('#speech-bubble i').removeClass()
         .addClass('icon').addClass(iconMap[data.topic]);
       $('.dimmer').removeClass('active').addClass('inactive');
 
     });
+  });
+
+  $(window).resize(function() {
+    var $button = $('#' + which);
+    var left = $button.offset().left - $('#speech-bubble').offset().left
+    var bw = $button.css('width');
+    var width = parseInt(bw.substr(0, bw.length - 2), 10);
+    var newpos = left - 40 + width / 2;
+    $('#triangle').css({ 'margin-left': newpos + 'px' });
   });
 
 });
